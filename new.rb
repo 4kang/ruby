fdf9a4254d8aaa3d881d00d07b0420ebecc4127e
4kang <106650699+4kang@users.noreply.github.com>
2022-06-06 05:50:33 +0000

    [Add]2-9

--- new.rb
+++ new.rb
@@ -21,9 +21,13 @@ car.move("右",5)
 
 # クラスメゾット
 class Car
+ def self.turn(direction)
+  puts "#{direction}に曲がります"
+ end
  def self.run(distance)
   puts "車で#{distance}キロ走ります。"
  end
 end
 
+Car.turn("右")
 Car.run(10)
